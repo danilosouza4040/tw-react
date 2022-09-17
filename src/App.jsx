81c51c67b6b84b5e1c4c75b5c94538c66ee9aec2
Danilo <danilosouza4040@gmail.com>
2022-09-16 23:32:15 -0300

    Arquivos Iniciais

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MainContent from './components/MainContent';
+import './styles/components/app.sass';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Base</h1>
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import Logo from '../assets/img/logo.webp';
+
+const MainContent = () => {
+  return (
+    <div>
+      <img src={Logo} alt="Logo" />
+    </div>
+  );
+};
+
+export default MainContent;
